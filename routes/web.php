ad15350980514a50fe8042fd13eb8de02bb3a5f7
Masbay-21 <sbayu4605@gmail.com>
2024-11-01 16:01:20 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('layouts.main');
+// });
+
+Route::get('/', action: [DashboardController::class, 'index'])->name(name:'dashboard');
